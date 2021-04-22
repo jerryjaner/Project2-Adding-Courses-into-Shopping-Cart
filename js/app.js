bee877a6f1595b79814f011a1d069efe76e21eeb
Jerry Janer <janerjerry99@gmail.com>
2021-04-22 18:07:19 +0800

    Print from Storage on Load

--- js/app.js
+++ js/app.js
@@ -18,6 +18,9 @@ loadEventListeners();
  // clear cart btn
  clearCartBtn.addEventListener('click', clearCart);
 
+ // Document Ready
+ document.addEventListener('DOMContentLoaded',getFromLocalStorage);
+
 
 
 }
@@ -120,4 +123,31 @@ function clearCart(){
     shoppingCartContent.removeChild(shoppingCartContent.firstChild);
 
    }
+}
+
+// Loads when document is ready and print courses into shopping cart 
+function getFromLocalStorage(){
+    let coursesLS = getCoursesFromStorage();
+
+    // Loop throught the courses and print into the cart
+    coursesLS.forEach(function(course){
+
+        // Create the <tr>
+        const row = document.createElement('tr');
+
+        // print the content
+        row.innerHTML = `
+        <tr>
+             <td>
+                    <img src = "${course.image}" width =100>
+                </td>
+                <td> ${course.title}</td>
+                <td> ${course.price}</td>
+                <td>
+                    <a href="#"class="remove" data-id="${course.id}">X</a>
+             </td>
+        </tr>
+        `;
+        shoppingCartContent.appendChild(row);
+    });
 }
